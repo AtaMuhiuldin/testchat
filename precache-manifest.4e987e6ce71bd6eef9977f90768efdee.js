4436e25d0072e4cbb5ec9d1118830033ad663be4
atamuhiuldin <atamuhiuldin@gmail.com>
2020-04-06 20:12:03 +0500

    Updates

---
+++ precache-manifest.4e987e6ce71bd6eef9977f90768efdee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf373b7e60827c4d54c689f8173565fc",
+    "url": "/testchat/index.html"
+  },
+  {
+    "revision": "a4c686e5701c45cf3d0f",
+    "url": "/testchat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cea5bd2c308fce2bdc8a",
+    "url": "/testchat/static/js/2.692a28eb.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/testchat/static/js/2.692a28eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c686e5701c45cf3d0f",
+    "url": "/testchat/static/js/main.828b10b8.chunk.js"
+  },
+  {
+    "revision": "3c4e8af02a05e2feb3cb",
+    "url": "/testchat/static/js/runtime-main.6d7f9a86.js"
+  }
+]);
